f4abe26d84afb0ded308e61d97309d04560e157a
Pedro Gabriel <pee.torres@gmail.com>
2021-08-29 13:47:35 -0300

    Created Base application project

---
+++ MyBestCoffee/MyBestCoffee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBestCoffee
+//
+//  Created by Pedro Gabriel on 29/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
