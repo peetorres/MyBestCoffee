376b6a393e7b31f065b4ef15ec1d6c114975db73
Pedro Gabriel <pee.torres@gmail.com>
2021-08-29 14:32:18 -0300

    🧩: Updated details scene into project

---
+++ MyBestCoffee/MyBestCoffee/Scenes/Details/DetailsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsViewController.swift
+//  MyBestCoffee
+//
+//  Created by Pedro Gabriel on 29/08/21.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    // MARK: Properties
+    
+    // MARK: Outlets
+    
+    // MARK: Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Coffee Shop Details"
+    }
+    
+    // MARK: Actions
+    
+    // MARK: Methods
+}
